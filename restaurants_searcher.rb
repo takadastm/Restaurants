c7916db0f3501647d3ace8849fe4da6cda9f93c9
EC2 Default User <ec2-user@ip-172-31-47-30.ap-northeast-1.compute.internal>
2020-11-28 02:51:29 +0000

    add column

--- restaurants_searcher.rb
+++ restaurants_searcher.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'json'
+require "csv"
 
 # 初期値
 KEYID = "c16b6b597258c69d1c64781aa0e15728" # 2021/02/20まで利用可能
@@ -10,7 +11,7 @@ FREEWORD = "渋谷駅"
 PARAMS = {"keyid":KEYID, "hit_per_page":HIT_PER_PAGE, "pref":PREF, "freeword_condition":FREEWORD_CONDITION, "freeword":FREEWORD}
 
 def write_data_to_csv(params)
-    restaurants = []
+    restaurants = [["名称","住所","営業日","電話番号"]]
     uri = URI.parse("https://api.gnavi.co.jp/RestSearchAPI/v3/")
     uri.query = URI.encode_www_form(PARAMS)  
     json_res = Net::HTTP.get uri
@@ -21,12 +22,14 @@ def write_data_to_csv(params)
         puts "エラーが発生しました！"
     end
     for restaurant in response["rest"] do
-        rest_name = restaurant["name"]
-        restaurants.append(rest_name)
+        rest_info = [restaurant["name"], restaurant["address"], restaurant["opentime"], restaurant["tel"]]
+        puts rest_info
+        restaurants.append(rest_info)
     end
     
-    File.open("restaurants_list.csv", "w") do |file|
-        file << restaurants
+    File.open("restaurants_list.csv", "w") do |csv|
+        restaurants.each do |rest_info|
+            csv << rest_info
     end
     return puts restaurants
 end
